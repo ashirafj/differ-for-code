50c21fe4f224df1fedea35228b33c8bb4c1657c0
Atsushi SHIRAFUJI <atsushirafuji@gmail.com>
2022-11-27 07:04:09 +0900

    Fix setup

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='differ-for-code',
-    version='1.0.0',
-    packages=find_packages()
+    name="differ-for-code",
+    version="1.0.1",
+    packages=find_packages("differ_for_code"),
+    install_requires=["colorama"],
 )
